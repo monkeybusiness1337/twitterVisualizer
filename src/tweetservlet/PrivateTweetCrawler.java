32860cccca81f02e1edbc4274adc780620103766
Max Haberbusch <max.haberbusch@gmail.com>
2015-01-13 11:50:11 +0100

    some changes

--- src/tweetservlet/PrivateTweetCrawler.java
+++ src/tweetservlet/PrivateTweetCrawler.java
@@ -12,10 +12,10 @@ public class PrivateTweetCrawler extends TweetCrawler {
 		super.twitter = twitter;
 	}
 	//Cannot test because no twitter to add in constructor
-	/*public static void main(String args[]){
-		PrivateTweetCrawler ts = new PrivateTweetCrawler();
-		String tweets = ts.getTweets(new Location(48.20732f,16.373792f), "asd", MediaType.PHOTO, false) ;
-		System.out.print(tweets) ;
-	}*/
+	public static void main(String args[]){
+		TweetCrawler ts = new TweetCrawler();
+		//String tweets = ts.getTweets(new Location(48.20732f,16.373792f), "asd", MediaType.PHOTO, false) ;
+		System.out.print(ts.getTrends()) ;
+	}
 	
 }
